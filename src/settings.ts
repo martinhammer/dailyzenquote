707200f4e87397e3368151e52b218b35ddd8ad50
Martin Hammer <me@martinhammer.com>
2026-09-08 14:25:35 +0200

    Fix for toast message
    Signed-off-by: Martin Hammer <me@martinhammer.com>

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import Settings from './Settings.vue'
 
+import '@nextcloud/dialogs/style.css'
+
 document.addEventListener('DOMContentLoaded', () => {
 	const el = document.getElementById('dailyzenquote-settings')
 	if (el === null) {
